dcda9489e4058daf5dac04153f7dab1391ed9655
srossi42 <ross.savinien@gmail.com>
2019-09-07 18:57:27 +0200

    fix size + def get_solution

--- puzzle_class.py
+++ puzzle_class.py
@@ -6,8 +6,14 @@ class Puzzle:
     g = 0
     h = 0
     parent = None
+    size = 0
 
     def __init__(self, parent=None):
         if parent:
             self.parent = parent
             self.g = parent.g + 1
+
+# A STAR ALGO
+
+    def get_solution(self):
+        return 1
