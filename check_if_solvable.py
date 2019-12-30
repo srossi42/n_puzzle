bcd286a7d03b2a3d9e0ff4b96139579ffbc72995
Charles-antoine PAQUET <cpaquet@e1r6p3.42.fr>
2019-12-30 18:16:15 +0100

    minor changes (func => class)

--- check_if_solvable.py
+++ check_if_solvable.py
@@ -1,56 +1,58 @@
 import fileinput
-
 import numpy as np
 import math as math
 
 
-size = 0
-blank_position = 0
-
-puzzle = np.array([1,8,2,0,4,3,7,6,5])
-blank_position = 4
 #puzzle = np.array([13,2,10,3,1,12,8,4,5,0,9,6,15,14,11,7])
-#blank_position = 10
+#zero_position = 10
 #puzzle = np.array([6,13,7,10,8,9,11,0,15,2,12,5,14,3,1,4])
-#blank_position = 8
+#zero_position = 8
 #puzzle = np.array([3,9,1,15,14,11,4,6,13,0,10,12,2,7,8,5])
-#blank_position = 10
+#zero_position = 10
 
-size = len(puzzle)
+class solvable:
 
-# Compte le nombre d'inversions pour trier le tableau (eg. [2,3,1] = 2)
-def count_inversion(puzzle):
-    inversion = 0
-    index = 0
-    index_next = 0
-    while index < len(puzzle) - 1:
-        if puzzle[index + 1] == 0:
-            index_next = index + 2
-        else:
-            index_next = index + 1
-        if puzzle[index] > puzzle[index_next]:
-            inversion += 1
-            puzzle[index], puzzle[index_next] = puzzle [index_next], puzzle[index]
-            index = 0
-        else:
-            index += 1
-    return inversion % 2
+    puzzle = 0
+    size = 0
+    zero_position = 0
 
-# Vérifie la position de X sur une ligne pair ou impair en partant de la dernière ligne
-def check_blank_position():
-    position = (size - blank_position + 1)
-    if position % math.sqrt(size) == 0 :
-        line = position // math.sqrt(size)
-    else :
-        line = position // math.sqrt(size) + 1
-    return line % 2
+    def __init__(self, puzzle):
+        self.zero_position = puzzle.zero_position
+        self.puzzle = puzzle.state
+        self.size = puzzle.size
 
-# Vérifie si le tableau est pair ou impair et les conditions associées (position de X et nbre d'inversions)
-def check_if_solvable(puzzle):
-    if (size % 2 == 1) and count_inversion(puzzle) == 0 :
-        return 1
-    elif (size % 2 == 0) and count_inversion(puzzle) != check_blank_position() :
-        return 1
-    return 0
+    # Compte le nombre d'inversions pour trier le tableau (eg. [2,3,1] = 2)
+    def count_inversion(self):
+        inversion = 0
+        index = 0
+        index_next = 0
+        while index < len(self.puzzle) - 1:
+            if self.puzzle[index + 1] == 0:
+                index_next = index + 2
+            else:
+                index_next = index + 1
+            if self.puzzle[index] > self.puzzle[index_next]:
+                inversion += 1
+                self.puzzle[index], self.puzzle[index_next] = self.puzzle [index_next], self.puzzle[index]
+                index = 0
+            else:
+                index += 1
+        return inversion % 2
 
-print (check_if_solvable(puzzle))
+    # Vérifie la position de X sur une ligne pair ou impair en partant de la dernière ligne
+    def check_zero_position(self):
+        #position = (self.size - self.zero_position + 1)
+        #if position % math.sqrt(size) == 0 :
+        #    line = position // math.sqrt(size)
+        #else :
+        #    line = position // math.sqrt(size) + 1
+        #return line % 2
+        return (self.zero_position[1] + 1 % 2)
+
+    # Vérifie si le tableau est pair ou impair et les conditions associées (position de X et nbre d'inversions)
+    def check_if_solvable(self):
+        if (self.size % 2 == 1) and self.count_inversion() == 0 :
+            return True
+        elif (self.size % 2 == 0) and self.count_inversion() != self.check_zero_position() :
+            return True
+        return False
