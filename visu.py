9fc3935565ef28ce64a588aed09c4d1fbeb56c01
Charles-antoine PAQUET <cpaquet@e3r4p4.42.fr>
2020-01-13 17:29:31 +0100

    visu img OK

--- visu.py
+++ visu.py
@@ -13,25 +13,98 @@ class Visu:
     width_win = 0
     height_win = 0
     window_size = 0
-    solutions = []
-    box_list = []
+    step_to_solution = []
+    solution = 0
+    box_bg_list = []
+    box_coord_list = []
     prev_state = 0
     done = False
+    img = 0
 
-    def __init__(self, puzzle):
-        self.solutions = puzzle
-        self.size = len(puzzle[0])
+    def __init__(self, step_to_solution, solution):
+        self.solution = solution
+        self.step_to_solution = step_to_solution
+        self.size = len(solution)
+        self.box_bg_list = [0] * (self.size * self.size)
+        self.box_coord_list = [0] * (self.size * self.size)
         self.puzzle_size = self.size_box * self.size
         self.width_win = self.puzzle_size + self.side_margin
         self.height_win = self.puzzle_size + self.header + self.footer
         self.window_size = [self.width_win, self.height_win]
         self.create_screen()
-        self.init_puzzle()
+        self.load_img()
 
-    def init_puzzle(self):
-        self.prev_state = self.solutions.pop(0)
-        self.calc_puzzle(self.prev_state)
-        self.draw_puzzle()
+    def load_img(self):
+        self.img = pygame.image.load("./img2.jpg")
+        self.img = pygame.transform.scale(self.img, (self.puzzle_size, self.puzzle_size))
+
+    def gen_img_bg(self):
+        for line in range(self.size):
+            x = line * self.size_box
+            for column in range(self.size):
+                y = column * self.size_box
+                value = self.solution[line][column]
+                bg = pygame.Surface((self.size_box, self.size_box))
+                bg.blit(self.img , (0,0), (y,x,100,100))
+                self.box_bg_list[value] = bg
+
+    def gen_nbr_bg(self):
+        for value in range(self.size * self.size):
+            bg = pygame.Surface((self.size_box, self.size_box))
+            bg.fill(self.grey)
+            pygame.draw.rect(bg, (255,255,255), bg.get_rect(topleft=(0, 0)), 1)
+            arialfont = pygame.font.Font(None, 40)
+            text = arialfont.render(str(value), True, (255, 255, 255), self.grey)
+            rect = text.get_rect(center=((self.size_box) / 2, (self.size_box) / 2))
+            bg.blit(text, rect)
+            self.box_bg_list[value] = bg
+
+    def calc_coord(self, puzzle):
+        for column in range(self.size):
+            x = (column * self.size_box) + self.side_margin / 2
+            for line in range(self.size):
+                y = (line * self.size_box) + self.header
+                value = puzzle[line][column]
+                coord = pygame.Rect((x,y), (self.size_box, self.size_box))
+                self.box_coord_list[value] = coord
+
+    def draw_puzzle(self):
+        self.screen.fill((255, 255, 255))
+        for value in range(self.size * self.size):
+            self.screen.blit(self.box_bg_list[value], self.box_coord_list[value])
+        self.draw_header()
+        self.draw_footer()
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.done = True
+    
+    def find_change(self, new_state):
+        value = 0
+        for line in range(self.size):
+            for column in range(self.size):
+                prev_value = self.prev_state[line][column]
+                new_value = new_state[line][column]
+                if  new_value != 0 and prev_value != new_value:
+                    new_line = line
+                    new_col = column
+                    value = new_value
+                if new_value == 0:
+                    old_line = line
+                    old_col = column
+        move_vert = new_line - old_line
+        move_hor = new_col - old_col
+        self.prev_state = new_state
+        return value, move_vert, move_hor
+
+    def move(self, value, move_vert, move_hor):
+        speed = 4
+        count = 0
+        while count < self.size_box :
+            self.box_coord_list[0] = self.box_coord_list[0].move(-move_hor * speed, -move_vert * speed)
+            self.box_coord_list[value] = self.box_coord_list[value].move(move_hor * speed, move_vert * speed)
+            count = count + speed 
+            self.draw_puzzle()
 
     def create_screen(self):
         pygame.init()
@@ -55,84 +128,30 @@ class Visu:
         rect = text.get_rect(center=(self.width_win / 2, self.height_win - self.footer / 2))
         self.screen.blit(text, rect)
 
-    def calc_puzzle(self, puzzle):
-        for line in range(self.size):
-            x = (line * self.size_box) + self.header
-            for column in range(self.size):
-                y = (column * self.size_box) + self.side_margin / 2
-                value = str(puzzle[line][column])
-                if (value != '0'):
-                    box, box_rect, value = self.create_box(value, x, y)
-                    self.append_box(box, box_rect, value)
-            
-    def create_box(self, value, x, y):
-        box = pygame.Surface((self.size_box, self.size_box))
-        box.fill(self.grey)
-        box_rect = box.get_rect(topleft=(y, x))
-        pygame.draw.rect(box, (255,255,255), box.get_rect(topleft=(0, 0)), 1)
-        arialfont = pygame.font.Font(None, 40)
-        text = arialfont.render(value, True, (255, 255, 255), self.grey)
-        rect = text.get_rect(center=((self.size_box) / 2, (self.size_box) / 2))
-        box.blit(text, rect)
-        return box, box_rect, value
-
-    def append_box(self, box, box_rect, value):
-        self.box_list.append((box, box_rect, value))
-
-    def draw_puzzle(self):
-        self.screen.fill((255, 255, 255))
-        for box,box_rect,value in self.box_list:
-            self.screen.blit(box, box_rect)
-        self.draw_header()
-        self.draw_footer()
-        pygame.display.flip()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.done = True
-    
-    def move_box(self, value, line, column):
-        x = (line * self.size_box) + self.header
-        y = (column * self.size_box) + self.side_margin / 2
-        new_box = self.create_box(str(value), x, y)
-        for old_index in range(len(self.box_list)):
-            if self.box_list[old_index][2] == str(value):
-                break
-        dist_line = new_box[1][0] - self.box_list[old_index][1][0]
-        dist_col = new_box[1][1] - self.box_list[old_index][1][1]
-        while self.box_list[old_index][1][1] != new_box[1][1] or self.box_list[old_index][1][0] != new_box[1][0]:
-            old_box = self.box_list.pop(old_index)
-            old_rect = old_box[1]
-            old_rect = old_rect.move(dist_line / 25, dist_col / 25) 
-            self.box_list.insert(old_index, (old_box[0], old_rect, old_box[2]))
-            self.draw_puzzle()
-        self.box_list = []
-        self.calc_puzzle(self.prev_state)
-
-    def find_move(self, new_state):
-        for line in range(self.size):
-            for column in range(self.size):
-                prev_value = self.prev_state[line][column]
-                new_value = new_state[line][column]
-                if  new_value != 0 and prev_value != new_value:
-                    self.prev_state = new_state
-                    return new_value, line, column
-        return False    
-
     def display(self):
+        self.gen_img_bg()
+        #self.gen_nbr_bg()
         while not self.done:
-            for state in self.solutions:
-                value, line, column = self.find_move(state)
-                self.move_box(value, line, column)
-            self.solutions = []
+            for state in self.step_to_solution:
+                if self.prev_state == 0:
+                    self.calc_coord(state)
+                    self.draw_puzzle()
+                    self.prev_state = state
+                else:
+                    change, move_vert, move_hor = self.find_change(state)
+                    self.move(change, move_vert, move_hor)
+            self.step_to_solution = []
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.done = True
         pygame.quit()
 
-npuzzle1 = [[1, 8, 2], [0, 4, 3], [7, 6, 5]]
-npuzzle2 = [[0, 8, 2], [1, 4, 3], [7, 6, 5]]
-npuzzle3 = [[8, 0, 2], [1, 4, 3], [7, 6, 5]]
-npuzzle4 = [[8, 4, 2], [1, 0, 3], [7, 6, 5]]
+npuzzle1 = [[0, 1, 2], [8, 4, 3], [7, 6, 5]]
+npuzzle2 = [[1, 0, 2], [8, 4, 3], [7, 6, 5]]
+npuzzle3 = [[1, 2, 0], [8, 4, 3], [7, 6, 5]]
+npuzzle4 = [[1, 2, 3], [8, 4, 0], [7, 6, 5]]
+npuzzle5 = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
+solution = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
 if __name__ == "__main__":
     
     liste = []
@@ -140,6 +159,8 @@ if __name__ == "__main__":
     liste.append(npuzzle2)
     liste.append(npuzzle3)
     liste.append(npuzzle4)
-    Visu(liste).display()
+    liste.append(npuzzle5)
+    #liste.append(solution)
+    Visu(liste, solution).display()
     # pour le mouvement, mettre la box du chiffre a zero pour l'effacer et commencer a la redessiner petit a petit pour la faire avancer
     # Attention il ne faut plus faire la margin
